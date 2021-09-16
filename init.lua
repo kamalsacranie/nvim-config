b16a977675cf7db4429373d71103c3d314da259a
Kamal Sacranie <kamalsjunkmail@gmail.com>
2021-09-16 22:17:14 +0100

    Setup packer

---
+++ init.lua
@@ -0,0 +1,31 @@
+--[[  ___          ___       ___                ___          ___       ___     
+     /\  \        /\  \     /\  \              /\__\        /\__\     /\  \    
+    /::\  \      /::\  \    \:\  \            /::|  |      /:/  /     \:\  \   
+   /:/\:\  \    /:/\:\  \    \:\  \          /:|:|  |     /:/  /       \:\  \  
+  /::\~\:\  \  /::\~\:\  \   /::\  \        /:/|:|  |__  /:/  /  ___   /::\  \ 
+ /:/\:\ \:\__\/:/\:\ \:\__\ /:/\:\__\      /:/ |:| /\__\/:/__/  /\__\ /:/\:\__\
+ \:\~\:\ \/__/\/__\:\/:/  //:/  \/__/      \/__|:|/:/  /\:\  \ /:/  //:/  \/__/
+  \:\ \:\__\       \::/  //:/  /               |:/:/  /  \:\  /:/  //:/  /     
+   \:\ \/__/       /:/  / \/__/                |::/  /    \:\/:/  / \/__/      
+    \:\__\        /:/  /                       /:/  /      \::/  /             
+     \/__/        \/__/                        \/__/        \/__/          --]]
+
+
+-- Boostrapping Packer
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
+-- Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 0
+vim.opt.expandtab = true
+
+require("plugins")
---
+++ lua/plugins.lua
@@ -0,0 +1,9 @@
+return require('packer').startup(function()
+
+    -- Meta updation
+    use 'wbthomason/packer.nvim'
+
+    -- Surround things with things duh
+    use 'tpope/vim-surround'
+
+end)
