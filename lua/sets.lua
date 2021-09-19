11b14a3f201642cf3e03cd78446673078c279b56
Kamal Sacranie <kamalsjunkmail@gmail.com>
2021-09-19 21:32:50 +0100

    minor

--- lua/sets.lua
+++ lua/sets.lua
@@ -8,6 +8,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 0
 vim.opt.expandtab = true
-
+-- Misc
 vim.opt.signcolumn = 'yes'
 vim.opt.termguicolors = true
+vim.cmd [[set viminfo+=n~/.cache/nvim/viminfo]]
