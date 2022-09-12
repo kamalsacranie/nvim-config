5745b18bdba2e7c83dd8822f264432f5bc641ca1
Kamal Sacranie <kamalsjunkmail@gmail.com>
2022-09-12 22:27:40 +0100

    Null ls updates

--- lua/user/lsp/null_ls.lua
+++ lua/user/lsp/null_ls.lua
@@ -1,17 +1,28 @@
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
+local hover = null_ls.builtins.hover
 
+-- Perhaps we should do a filetype specific sourcing thing in our after/ directory??
 local sources = {
 	formatting.stylua.with({
 		extra_args = { "--column-width", "80" },
 	}),
-	-- formatting.prettier.with({
-	--     disabled_filetypes = { "markdown", "rmd", "md", "markdown.pandoc", "rmarkdown" },
-	-- }),
+	formatting.prettier.with({
+		disabled_filetypes = {
+			"markdown",
+			"rmd",
+			"md",
+			"markdown.pandoc",
+			"rmarkdown",
+		},
+	}),
 	-- Markdown linting with MD025 disbaled so we can have mutiple levle one
 	-- headings
-	formatting.markdownlint,
+	formatting.markdownlint.with({
+		filetypes = { "markdown", "anki" },
+	}),
 	diagnostics.markdownlint.with({
 		extra_args = { "--disable", "MD025" },
 		filetypes = { "markdown", "anki" },
@@ -29,6 +40,10 @@ local sources = {
 	}),
 	-- Shell formatting
 	formatting.shfmt,
+	code_actions.gitsigns, -- soooo useful
+	hover.dictionary.with({
+        filetypes = {"markdown", "anki", "rmarkdown", "text", "gitcommit"}
+    }),
 }
 
 null_ls.setup({
