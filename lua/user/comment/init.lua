351c7ed6aa4d1459a3f32f2f1d51229e8d9c5277
Kamal Sacranie <kamalsjunkmail@gmail.com>
2022-09-12 22:24:24 +0100

    Reformat comment.nvim

--- lua/user/comment/init.lua
+++ lua/user/comment/init.lua
@@ -1,24 +1,29 @@
-local status_ok, comment = pcall(require, "Comment")
+local status_ok, comment = load_package("comment")
 if not status_ok then
-    return
+	return
 end
 
-comment.setup {
-    -- This prehook is tacken from their readme. It intergrates with
-    -- ts_context_commentstring
-    pre_hook = function(ctx)
-        local U = require "Comment.utils"
+comment.setup({
+	-- this prehook is tacken from their readme. it intergrates with
+	-- ts_context_commentstring
+	pre_hook = function(ctx)
+		-- linking in our context_comments plugin
+		require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+		local u = require("comment.utils")
 
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-            location = require("ts_context_commentstring.utils").get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-            location = require("ts_context_commentstring.utils").get_visual_start_location()
-        end
+		-- local type = ctx.ctype == u.ctype.linewise and "__default" or "__multiline"
+		local location = nil
+		if ctx.ctype == u.ctype.blockwise then
+			location =
+				require("ts_context_commentstring.utils").get_cursor_location()
+		elseif ctx.cmotion == u.cmotion.v or ctx.cmotion == u.cmotion.v then
+			location =
+				require("ts_context_commentstring.utils").get_visual_start_location()
+		end
 
-        return require("ts_context_commentstring.internal").calculate_commentstring {
-            key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-            location = location,
-        }
-    end,
-}
+		return require("ts_context_commentstring.internal").calculate_commentstring({
+			key = ctx.ctype == u.ctype.line and "__default" or "__multiline",
+			location = location,
+		})
+	end,
+})
