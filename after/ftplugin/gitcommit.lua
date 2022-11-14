bbb4c43a94b4cc31859786d973e642bc73be26c2
Kamal Sacranie <kamalsjunkmail@gmail.com>
2022-11-14 10:25:30 +0000

    Minor fix to gitcommit filetype

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,5 +1,17 @@
-options_set({
-	colorcolumn = "73",
+-- Can't use my QOL function here for some reason
+vim.cmd([[setlocal colorcolumn=73]])
+
+local create_augroup = vim.api.nvim_create_augroup
+local create_autocmd = vim.api.nvim_create_autocmd
+
+local gitcommit_cleanup = create_augroup("gitcommit_cleanup", { clear = true })
+create_autocmd("BufEnter", {
+	group = gitcommit_cleanup,
+	callback = function()
+		vim.cmd([[UltiSnipsAddFiletypes markdown.markdown_core]])
+	end,
+	pattern = "gitcommit",
+	desc = "Gitcommit cleanup when we go into buffer",
 })
 
 require("utils.spell")
