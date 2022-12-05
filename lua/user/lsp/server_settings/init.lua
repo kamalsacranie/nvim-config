0ff8a65da1c3e1f1bada084c30fb0ffa9be95f69
Kamal Sacranie <kamalsjunkmail@gmail.com>
2022-12-05 00:30:37 +0000

    addition to lsp optimisation

--- lua/user/lsp/server_settings/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-    sumneko_lua = require("user.lsp.server_settings.sumneko_lua"),
-    pyright = require("user.lsp.server_settings.pyright"),
-    texlab = require('user.lsp.server_settings.texlab')
-}
