1893901b39179c2632d77117438461cea94ab9b5
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-03-09 00:15:13 +0000

    Swap lsp_signature for noice signature

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,9 +10,6 @@ return {
 					["vim.lsp.util.stylize_markdown"] = true,
 					["cmp.entry.get_documentation"] = true,
 				},
-				signature = {
-					enabled = false,
-				},
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
--- lua/user/lsp/on_attach/init.lua
+++ lua/user/lsp/on_attach/init.lua
@@ -5,9 +5,6 @@ M.on_attach = function(client, bufnr)
 	-- setting up our mappings
 	require("user.mappings.lsp_map").lsp_mappings(bufnr, client)
 	require("user.lsp.on_attach.format_on_save").enable_format_on_save()
-	require("lsp_signature").on_attach(
-		require("user.lsp.on_attach.lsp_signature")
-	)
 	if client.server_capabilities.document_highlight then
 		-- This is detailed in the docs under helpf for document_highlight()
 		vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
