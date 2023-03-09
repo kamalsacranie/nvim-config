1b0bfa573e4c31ee2d52e7ffa119217dd49b82b3
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-03-09 01:45:54 +0000

    Refactor gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,8 +1,18 @@
+local function setup()
+    local loaded, gs = load_package("gitsigns")
+	if not loaded then
+		return
+	end
+	gs.setup({
+		on_attach = function(bufnr)
+			require("user.mappings.gitsigns_map").gitsigns_mappings(bufnr)
+		end,
+	})
+end
+
 return {
 	"lewis6991/gitsigns.nvim",
 	dependencies = "nvim-lua/plenary.nvim",
 	-- Only config required
-	config = function()
-		require("user.gitsigns")
-	end,
+	config = setup,
 }
