e2a8478dea2b7a1c6a565492fe5ae5760fe30f7f
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-03-09 01:54:00 +0000

    relocate lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,41 @@
 -- Status line
+local function setup()
+	local loaded, lualine = load_package("lualine")
+	if not loaded then
+		return
+	end
+
+	lualine.setup({
+		options = {
+			icons_enabled = true,
+			theme = "nightfly",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch" },
+			lualine_c = { "filename" },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = {
+				"progress",
+			},
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		extensions = {},
+	})
+end
 
 return {
-    "hoob3rt/lualine.nvim",
-    config = function()
-        require("user.lualine")
-    end,
+	"hoob3rt/lualine.nvim",
+	config = setup,
 }
