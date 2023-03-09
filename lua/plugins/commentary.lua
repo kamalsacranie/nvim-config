cb7c8e888c4e014be214e233aedaae53bf96ea01
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-03-09 00:01:34 +0000

    Move commentary config

--- lua/plugins/commentary.lua
+++ lua/plugins/commentary.lua
@@ -1,9 +1,20 @@
--- Commentary
+local setup = function()
+	local status_ok, comment = load_package("Comment")
+	if not status_ok then
+		return
+	end
 
+	-- Use treesitter grammar to put in the correct comments
+	comment.setup({
+		pre_hook = require(
+			"ts_context_commentstring.integrations.comment_nvim"
+		).create_pre_hook(),
+	})
+end
+
+-- Commentary
 return {
-    "numToStr/Comment.nvim",
-    config = function()
-        require("user.comment")
-    end,
-    dependencies = "JoosepAlviste/nvim-ts-context-commentstring"
+	"numToStr/Comment.nvim",
+	config = setup(),
+	dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
 }
