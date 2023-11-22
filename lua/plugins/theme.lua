9af81451a50bee6ac7008e4ce6bed4daa8a496aa
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-11-22 14:41:06 +0000

    update theeme settings to include markdown hl

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,52 +1,53 @@
 local themes = {
-    kanagawa = {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            require("kanagawa").setup({})
-            vim.cmd [[colorscheme kanagawa]]
-        end
-    },
-    catppuccin = {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                term_colors = false,         -- sets terminal colors (e.g. `g:terminal_color_0`)
-                dim_inactive = {
-                    enabled = true,          -- dims the background color of inactive window
-                },
-                styles = {                   -- Handles the styles of general hi groups (see `:h highlight-args`):
-                    comments = { "italic" }, -- Change the style of comments
-                    conditionals = { "italic" },
-                    loops = {},
-                    functions = {},
-                    keywords = {},
-                    strings = {},
-                    variables = {},
-                    numbers = {},
-                    booleans = { "bold" },
-                    properties = {},
-                    types = {},
-                    operators = {},
-                },
-                custom_highlights = {},
-                integrations = {
-                    -- https://github.com/catppuccin/nvim#integrations
-                    cmp = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                    treesitter = true,
-                    notify = true,
-                    mini = {
-                        enabled = true,
-                        indentscope_color = "",
-                    },
-                },
-            })
-            vim.cmd [[colorscheme catppuccin]]
-        end
-    }
+	kanagawa = {
+		"rebelot/kanagawa.nvim",
+		config = function()
+			require("kanagawa").setup({})
+			vim.cmd([[colorscheme kanagawa]])
+		end,
+	},
+	catppuccin = {
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+				dim_inactive = {
+					enabled = true, -- dims the background color of inactive window
+				},
+				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+					comments = { "italic" }, -- Change the style of comments
+					conditionals = { "italic" },
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = { "bold" },
+					properties = {},
+					types = {},
+					operators = {},
+				},
+				custom_highlights = {},
+				integrations = {
+					-- https://github.com/catppuccin/nvim#integrations
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = true,
+					markdown = true,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+				},
+			})
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
 
 return themes.catppuccin
