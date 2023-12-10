ae8f90e386952eb1971ba9620955c6a8c7545da0
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-12-10 18:01:41 +0000

    update luasnip config

--- lua/plugins/luasnip/init.lua
+++ lua/plugins/luasnip/init.lua
@@ -1,13 +1,12 @@
-local utils = require("nvim-mapper")
+local mapper = require("nvim-mapper")
 
 local setup = function()
     local ls = load_package("luasnip")
     if not ls then
         return
     end
-
     local ls_ft = require("luasnip.extras.filetype_functions")
-    require("luasnip.loaders.from_lua").load({
+    require("luasnip.loaders.from_lua").lazy_load({
         paths = {
             "./after/ftsnippets/lua",
         },
@@ -26,13 +25,14 @@ local setup = function()
         link_roots = true,
         link_children = true,
         enable_autosnippets = true,
-        ft_func = ls_ft.from_filetype,
+        ft_func = ls_ft.from_cursor_pos,
+        snip_env = require("plugins.luasnip.snip_env_ext"),
     }
-    utils.map_keymap_list(require("plugins.luasnip.mappings"))
+    mapper.map_keymap_list(require("plugins.luasnip.mappings"))
 
-    -- require("user.mappings.luasnip_map")
-    -- ls.config.setup(extend_config(defaults, "luasnip_config_extend"))
-    ls.config.setup(defaults)
+    local config = vim.tbl_deep_extend("force", defaults,
+        get_table_from_ftplugin_filtype("luasnip"))
+    ls.config.setup(config)
 end
 
 return {
@@ -41,5 +41,5 @@ return {
     config = setup,
     event = { "InsertEnter" },
     build = "make install_jsregexp",
-    dependencies = { "rafamadriz/friendly-snippets" }
+    dependencies = { { "rafamadriz/friendly-snippets" }, { "nvim-treesitter/nvim-treesitter" } }
 }
