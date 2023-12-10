5796004966a75283e1b9b14d5f132e312d3770e6
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-12-10 18:03:25 +0000

    update markdown query for latex math

--- queries/markdown/injections.scm
+++ queries/markdown/injections.scm
@@ -1,4 +1,3 @@
 ;; inherits: markdown
 ;; extends
-((inline) @latex (#match? @latex "\\\\begin\\{.*\\}(\\n|.|\\r|\\r\\n)*\\\\end\\{.*\\}"))
 (((inline) @latex (#match? @latex "\\\\begin\\{.*\\}(\\n|.|\\r|\\r\\n)*\\\\end\\{.*\\}")) @injection.content (#set! injection.language "latex"))
