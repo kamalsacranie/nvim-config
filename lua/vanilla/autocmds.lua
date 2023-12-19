30878644913569eb1e06320b0d58dd052af5153b
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-12-19 22:17:04 +0000

    fix terminal autocmd error for folds

--- lua/vanilla/autocmds.lua
+++ lua/vanilla/autocmds.lua
@@ -1,23 +1,28 @@
 -- Restore last cursor position
 vim.api.nvim_create_augroup("vim_startup", { clear = true })
 vim.api.nvim_create_autocmd("BufReadPost", {
-	group = "vim_startup",
-	pattern = "*",
-	command = [[
+    group = "vim_startup",
+    pattern = "*",
+    command = [[
         if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'
         exe "normal! g`\""
         endif"
     ]],
-	desc = "Restore prevous cursor position on opening a file",
+    desc = "Restore prevous cursor position on opening a file",
 })
 
 -- Preserving our folds
 vim.api.nvim_create_augroup("remember_folds", { clear = true })
 vim.api.nvim_create_autocmd(
-	"BufWinLeave",
-	{ group = "remember_folds", pattern = "*.*", command = "mkview" }
+    "BufWinLeave",
+    {
+        group = "remember_folds",
+        pattern = "*.*",
+        command =
+        "if &buftype !=# 'terminal' | mkview | endif"
+    }
 )
 vim.api.nvim_create_autocmd(
-	"BufWinEnter",
-	{ group = "remember_folds", pattern = "*.*", command = "silent! loadview" }
+    "BufWinEnter",
+    { group = "remember_folds", pattern = "*.*", command = "silent! loadview" }
 )
