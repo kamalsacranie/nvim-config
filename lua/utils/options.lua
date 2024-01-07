78f587a9dd9a63825ef4828ae54f24564c1e410e
Kamal Sacranie <kamalsjunkmail@gmail.com>
2024-01-07 23:04:33 +0000

    Make option setter utils more FUNCTIONAL

--- lua/utils/options.lua
+++ lua/utils/options.lua
@@ -6,10 +6,24 @@ local M = {}
 
 ---@param default_opts vim.api.keyset.option
 ---@return fun(key: string, value: any, opts?: vim.api.keyset.option)
-local set_option_value_with_defaults = function(default_opts)
+local set_opt_with_defaults = function(default_opts)
     return function(key, value, opts)
         vim.api.nvim_set_option_value(key, value,
             vim.tbl_deep_extend("force", default_opts, opts or {}) or {})
+    end
+end
+
+--- Returns a function which sets all options with a given setting function. If
+--- no setting function is given, vim.api.nvim_set_option_value is used.
+---@param options vim.bo | vim.wo
+---@param setting_func? fun(key: string, value: any, opts?: vim.api.keyset.option)
+---@return fun(opts?: vim.api.keyset.option)
+M.batch_set_options = function(options, setting_func)
+    setting_func = setting_func or vim.api.nvim_set_option_value
+    return function(opts)
+        for key, value in pairs(options) do
+            setting_func(key, value, opts or {})
+        end
     end
 end
 
@@ -17,10 +31,9 @@ end
 ---@param buf_options vim.bo
 ---@param opts? vim.api.keyset.option
 M.set_buf_options = function(buf_options, opts)
-    for key, value in pairs(buf_options) do
-        set_option_value_with_defaults({ buf = vim.fn.bufnr() })(
-            key, value, opts)
-    end
+    local option_setter = M.batch_set_options(buf_options,
+        set_opt_with_defaults({ buf = vim.fn.bufnr() }))
+    option_setter(opts)
 end
 
 
@@ -28,13 +41,20 @@ end
 ---@param win_options vim.wo
 ---@param opts? vim.api.keyset.option
 M.set_window_options = function(win_options, opts)
-    for key, value in pairs(win_options) do
-        set_option_value_with_defaults({
+    local options_setter = M.batch_set_options(win_options,
+        set_opt_with_defaults({
             win = vim.fn
                 .win_getid(),
             scope = "local"
-        })(key, value, opts)
-    end
+        }))
+    options_setter(opts)
+end
+
+---@param global_options vim.bo
+---@param opts? vim.api.keyset.option
+M.set_global_options = function(global_options, opts)
+    local options_setter = M.batch_set_options(global_options)
+    options_setter(opts)
 end
 
 ---Batch set options table. Both window and buffer set with local as default
