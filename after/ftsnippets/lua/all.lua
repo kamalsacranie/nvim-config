7a386352b0029b343310d60ba49e6022727d8ac9
Kamal Sacranie <kamalsjunkmail@gmail.com>
2024-01-12 12:51:12 +0000

    Add some snippets

---
+++ after/ftsnippets/lua/all.lua
@@ -0,0 +1,9 @@
+return {
+    -- s("selected_text", f(function(args, snip)
+    --     local res, env = {}, snip.env
+    --     table.insert(res,
+    --         "Selected Text (current line is " .. env.TM_LINE_NUMBER .. "):")
+    --     for _, ele in ipairs(env.LS_SELECT_RAW) do table.insert(res, ele) end
+    --     return res
+    -- end, {}))
+}
--- after/ftsnippets/lua/markdown_core/inline.lua
+++ after/ftsnippets/lua/markdown_core/inline.lua
@@ -1,5 +1,5 @@
 return {
-    s("link", { t("["), i(1), t("]("), lsg.visual(), i(2), t(")"), i(0) }),
+    s("link", { t("["), i(1), t("]("), visual(), i(2), t(")"), i(0) }),
     s({ trig = "appen?d?i?x?(%d*)", name = "Appendix heading", regTrig = true },
         {
             f(function(_, snip)
@@ -12,27 +12,30 @@ return {
         }),
     s({ trig = "c'", name = "inline code" }, {
         t("`"),
-        lsg.visual(),
+        visual(),
         i(1),
         t("`"),
     }),
-    s({ trig = "b'", name = "bold" }, { t("**"), lsg.visual(), i(1), t("**") }),
-    lsg.midword({ trig = "fn", name = "footnote" }, {
-        t("^["),
-        lsg.visual(),
-        i(1),
-        t("]"),
-    }),
+    s({ trig = "b'", name = "bold" }, { t("**"), visual(), i(1), t("**") }),
     s(
         { trig = "i'", name = "italics" },
-        { t("*"), lsg.visual(), i(1), t("*") }
+        { t("*"), visual(), i(1), t("*") }
     ),
     s(
         { trig = "bi'", name = "bitalics" },
-        { t("***"), lsg.visual(), i(1), t("***") }
+        { t("***"), visual(), i(1), t("***") }
     ),
     s(
         { trig = "d'", name = "math" },
-        { t("$"), lsg.visual(), i(1), t("$"), i(2) }
+        { t("$"), visual(), i(1), t("$"), i(2) }
     ),
+    midword(
+        { trig = [[(.*)fn]], name = "footnote", trigEngine = "pattern", show_condition = function() return false end },
+        {
+            t("^["),
+            visual(),
+            i(1),
+            t("]"),
+        }
+    )
 }
